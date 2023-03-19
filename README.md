f2cf603f0c1530ded145e58a781f180432587078
Dan Häßler <dhaessler@itemis.com>
2023-03-19 04:25:21 +0100

    update setup, linux support

--- README.md
+++ README.md
@@ -1,19 +1,35 @@
-# sdl-win
-Building and compiling an example sdl2 project on Windows.
+# SDL2 C++ Example
+Building and compiling an example SDL2 C++ project with G++ and CMake/Make.
 
-# Install
-1. [Download SDL 2.26.4](https://github.com/libsdl-org/SDL/releases)
-2. Extract SDL 2.26.4 to `./SDL2`
+<img src="result.gif" alt="drawing" width="400"/>
 
-# Build & Compile
-### G++
-1. `cp ./SDL2/x86_64-w64-mingw32/bin/SDL2.dll ./SDL2.dll` (ONCE)
-2. `g++ main.cpp -Isdl2/x86_64-w64-mingw32/include/SDL2 -Lsdl2/x86_64-w64-mingw32/lib -Wall -lmingw32 -lSDL2main -lSDL2 -o main`
+## Windows
+---
+
+### Install
+*  [Download SDL 2.26.4](https://github.com/libsdl-org/SDL/releases)
+*  Extract SDL 2.26.4 to `./SDL2`
 
 ### CMake & Make
-1. `cmake -S . -B ./build -G "Unix Makefiles`
-2. `cd ./build`
-3. `make`
+*  `cmake -S . -B ./build -G "Unix Makefiles"`
+*  `cd ./build`
+*  `make`
+###  G++
+*  `cp ./SDL2/x86_64-w64-mingw32/bin/SDL2.dll ./SDL2.dll`
+*  `g++ main.cpp -Isdl2/x86_64-w64-mingw32/include/ -Lsdl2/x86_64-w64-mingw32/lib -Wall -lmingw32 -lSDL2main -lSDL2 -o main`
 
-# Result
-![Expected Image](expected.png)
+## Linux 
+---
+
+### Install
+* `sudo apt-get install libsdl2-dev`
+
+### CMake & Make
+*  `cmake -S . -B ./build -G "Unix Makefiles"`
+*  `cd ./build`
+*  `make`
+
+
+### G++
+*  `g++ main.cpp -Ilibsdl2 -Llibsdl2 -Wall -lSDL2main -lSDL2 -o main`
+
--- main.cpp
+++ main.cpp
@@ -1,35 +1,38 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <SDL2/SDL.h>
-#define WIDTH 640
-#define HEIGHT 480
+#include <set>
+
+#define WIDTH 1080
+#define HEIGHT 720
+
 #define SIZE 200
-#define SPEED 600
-#define GRAVITY 60
-#define FPS 60
-#define JUMP -1200
+#define SPEED 1000
+#define GRAVITY 100
+#define FPS 144
+#define JUMP -3000
 
 int main(int argc, char *argv[])
 {
-    /* Initializes the timer, audio, video, joystick,
-    haptic, gamecontroller and events subsystems */
+    /* Initialize SDL */
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
     {
         printf("Error initializing SDL: %s\n", SDL_GetError());
         return 0;
     }
-    /* Create a window */
-    SDL_Window *wind = SDL_CreateWindow("Hello Platformer!",
+
+    SDL_Window *wind = SDL_CreateWindow("Example SDL2!",
                                         SDL_WINDOWPOS_CENTERED,
                                         SDL_WINDOWPOS_CENTERED,
                                         WIDTH, HEIGHT, 0);
+
     if (!wind)
     {
         printf("Error creating window: %s\n", SDL_GetError());
         SDL_Quit();
         return 0;
     }
-    /* Create a renderer */
+
     Uint32 render_flags = SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC;
     SDL_Renderer *rend = SDL_CreateRenderer(wind, -1, render_flags);
     if (!rend)
@@ -39,9 +42,11 @@ int main(int argc, char *argv[])
         SDL_Quit();
         return 0;
     }
-    /* Main loop */
-    bool running = true, jump_pressed = false, can_jump = true,
-         left_pressed = false, right_pressed = false;
+
+    bool running = true;
+    bool can_jump = true;
+    std::set<uint32_t> pressed_keys;
+
     float x_pos = (WIDTH - SIZE) / 2, y_pos = (HEIGHT - SIZE) / 2, x_vel = 0, y_vel = 0;
     SDL_Rect rect = {(int)x_pos, (int)y_pos, SIZE, SIZE};
     SDL_Event event;
@@ -56,40 +61,10 @@ int main(int argc, char *argv[])
                 running = false;
                 break;
             case SDL_KEYDOWN:
-                switch (event.key.keysym.scancode)
-                {
-                case SDL_SCANCODE_SPACE:
-                    jump_pressed = true;
-                    break;
-                case SDL_SCANCODE_A:
-                case SDL_SCANCODE_LEFT:
-                    left_pressed = true;
-                    break;
-                case SDL_SCANCODE_D:
-                case SDL_SCANCODE_RIGHT:
-                    right_pressed = true;
-                    break;
-                default:
-                    break;
-                }
+                pressed_keys.insert(event.key.keysym.scancode);
                 break;
             case SDL_KEYUP:
-                switch (event.key.keysym.scancode)
-                {
-                case SDL_SCANCODE_SPACE:
-                    jump_pressed = false;
-                    break;
-                case SDL_SCANCODE_A:
-                case SDL_SCANCODE_LEFT:
-                    left_pressed = false;
-                    break;
-                case SDL_SCANCODE_D:
-                case SDL_SCANCODE_RIGHT:
-                    right_pressed = false;
-                    break;
-                default:
-                    break;
-                }
+                pressed_keys.erase(event.key.keysym.scancode);
                 break;
             default:
                 break;
@@ -98,38 +73,55 @@ int main(int argc, char *argv[])
         /* Clear screen */
         SDL_SetRenderDrawColor(rend, 0, 0, 0, 255);
         SDL_RenderClear(rend);
-        /* Move the rectangle */
-        x_vel = (right_pressed - left_pressed) * SPEED;
+        x_vel = ((pressed_keys.find(SDL_Scancode::SDL_SCANCODE_D) != pressed_keys.end()) - (pressed_keys.find(SDL_Scancode::SDL_SCANCODE_A) != pressed_keys.end())) * SPEED;
         y_vel += GRAVITY;
-        if (jump_pressed && can_jump)
+        if ((pressed_keys.find(SDL_Scancode::SDL_SCANCODE_SPACE) != pressed_keys.end()) && can_jump)
         {
             can_jump = false;
             y_vel = JUMP;
         }
-        x_pos += x_vel / 60;
-        y_pos += y_vel / 60;
+
+        x_pos += x_vel / FPS;
+        y_pos += y_vel / FPS;
+
         if (x_pos <= 0)
+        {
             x_pos = 0;
+        }
+
         if (x_pos >= WIDTH - rect.w)
+        {
             x_pos = WIDTH - rect.w;
+        }
+
         if (y_pos <= 0)
+        {
             y_pos = 0;
+        }
+
         if (y_pos >= HEIGHT - rect.h)
         {
             y_vel = 0;
             y_pos = HEIGHT - rect.h;
-            if (!jump_pressed)
+            if (pressed_keys.find(SDL_Scancode::SDL_SCANCODE_SPACE) == pressed_keys.end())
+            {
                 can_jump = true;
+            }
         }
+
+        /* Update rectangle position */
         rect.x = (int)x_pos;
         rect.y = (int)y_pos;
+
         /* Draw the rectangle */
         SDL_SetRenderDrawColor(rend, 255, 0, 0, 255);
         SDL_RenderFillRect(rend, &rect);
+
         /* Draw to window and loop */
         SDL_RenderPresent(rend);
         SDL_Delay(1000 / FPS);
     }
+
     /* Release resources */
     SDL_DestroyRenderer(rend);
     SDL_DestroyWindow(wind);
